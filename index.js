f9049025187af7ab8441780a2f0f55bb972906bc
Shraddhasaini <shraddhasaini99@gmail.com>
2020-05-08 09:42:19 +0530

    bot is ready

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
-require('dotenv').config();
+const { CommentStream } = require("snoostorm");
 
+require('dotenv').config();
 const Snoowrap = require('snoowrap');
 const Snoostorm = require('snoostorm');
 
 const r = new Snoowrap({
-    userAgent: 'reddit-bot-example-node',
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    username: process.env.REDDIT_USER,
-    password: process.env.REDDIT_PASS
+	userAgent: 'some-description',
+	clientId: process.env.CLIENT_ID,
+	clientSecret: process.env.CLIENT_SECRET,
+	username: process.env.REDDIT_USER,
+	password: process.env.REDDIT_PASS
 });
-const client = new Snoostorm(r);
 
-const streamOpts = {
-    subreddit: 'testingground4bots',
-    results: 25
-};
+const stream = new CommentStream(r, { subreddit: "all", results: 25 });
 
-const comments = client.CommentStream(streamOpts);
-comments.on('comment', (comment) => {
-  if (comment.body === 'nice') {
-  comment.reply('very nice');
-}
-});
+stream.on("item", comment => {
+    console.log(comment)
+})
