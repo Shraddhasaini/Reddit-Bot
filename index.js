8c3a061ddc91b51f823623f6da26d89cb8b120fc
Shraddhasaini <shraddhasaini99@gmail.com>
2020-05-08 09:30:37 +0530

    build snoostorm client

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ require('dotenv').config();
 
 const Snoowrap = require('snoowrap');
 const Snoostorm = require('snoostorm');
+
+const r = new Snoowrap({
+    userAgent: 'reddit-bot-example-node',
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    username: process.env.REDDIT_USER,
+    password: process.env.REDDIT_PASS
+});
+const client = new Snoostorm(r);
